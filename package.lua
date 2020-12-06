822e27ee1235c4aea757238a15f363e8ab8fa577
boversoneg <59316027+boversoneg@users.noreply.github.com>
2020-12-06 20:59:12 +0100

    added homepage
    sometimes package.lua without homepage doing some errors

--- package.lua
+++ package.lua
@@ -2,6 +2,7 @@ return {
   name = "date",
   version = "0.0.1",
   description = "A collection of date manipulation helpers",
+  homepage = 'https://github.com/dvv/luvit-date',
   author = "Vladimir Dronnikov <dronnikov@gmail.com>",
   main = "date.lua",
 }
