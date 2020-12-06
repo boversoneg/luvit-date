f8779143a6bf1bfc66dab60f999fb6a7bb66411b
boversoneg <59316027+boversoneg@users.noreply.github.com>
2020-12-06 21:01:20 +0100

    Added homepage and reworked name

--- package.lua
+++ package.lua
@@ -1,7 +1,8 @@
 return {
-  name = "date",
+  name = "dvv/luvit-date",
   version = "0.0.1",
   description = "A collection of date manipulation helpers",
+  homepage = 'https://github.com/dvv/luvit-date',
   author = "Vladimir Dronnikov <dronnikov@gmail.com>",
   main = "date.lua",
 }
